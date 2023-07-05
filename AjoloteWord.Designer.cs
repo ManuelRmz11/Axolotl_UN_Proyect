3ea9340d0f93e878b451b5cd868b3947e6bbe6d9
ManuelRmz11 <116410273+ManuelRmz11@users.noreply.github.com>
2023-07-05 01:58:12 -0600

    Moises3

--- AjoloteWord.Designer.cs
+++ AjoloteWord.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // btnRegresar
             // 
-            this.btnRegresar.Location = new System.Drawing.Point(1360, 13);
+            this.btnRegresar.Location = new System.Drawing.Point(984, 12);
             this.btnRegresar.Margin = new System.Windows.Forms.Padding(4);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(174, 51);
@@ -51,7 +51,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(13, 425);
+            this.label1.Location = new System.Drawing.Point(13, 378);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(929, 132);
@@ -63,7 +63,7 @@
             this.pictureBox1.Image = global::Axolotl_UN_Proyect.Properties.Resources.jpg3;
             this.pictureBox1.Location = new System.Drawing.Point(17, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(402, 397);
+            this.pictureBox1.Size = new System.Drawing.Size(363, 350);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -73,7 +73,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Axolotl_UN_Proyect.Properties.Resources.Font1;
-            this.ClientSize = new System.Drawing.Size(1547, 635);
+            this.ClientSize = new System.Drawing.Size(1171, 539);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnRegresar);
