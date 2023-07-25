a82150aeee9393485a7262ae83899551683e4338
Alejandro Reyna <alexreyna115@gmail.com>
2023-07-25 17:13:40 -0600

    Actualizacion

--- AjoloteAdventures02.cs
+++ AjoloteAdventures02.cs
@@ -29,10 +29,5 @@ namespace Axolotl_UN_Proyect
             this.Hide();
             Form1.Show();
         }
-
-        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
-        {
-
-        }
     }
 }
