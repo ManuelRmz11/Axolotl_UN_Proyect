1a505ffcd6694c1cbe1c5631a074f31a2bd07f45
ManuelRmz11 <116410273+ManuelRmz11@users.noreply.github.com>
2023-07-25 12:51:11 -0600

    Añadi notas

--- AjoloteWord.cs
+++ AjoloteWord.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection.Emit;
@@ -13,8 +14,8 @@ namespace Axolotl_UN_Proyect
 {
     public partial class AjoloteWord : Form
     {
-        private Timer timer;
-        private Timer timer2;
+        private Timer timer; // Temporizador para cambiar el contenido después de cierto tiempo
+        private Timer timer2; // Temporizador para cambiar el contenido después de otro tiempo
 
         public AjoloteWord()
         {
@@ -24,20 +25,25 @@ namespace Axolotl_UN_Proyect
 
         private void AjoloteWord_Load(object sender, EventArgs e)
         {
+            // Configuración del primer temporizador
             timer = new Timer();
             timer.Interval = 10000;  // 10 segundos
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            timer.Tick += Timer_Tick; // Evento que se ejecutará cada vez que pase el tiempo establecido
+            timer.Start(); // Iniciar el temporizador
 
+            // Configuración del segundo temporizador
             timer2 = new Timer();
             timer2.Interval = 20000;  // 20 segundos
-            timer2.Tick += Timer2_Tick;
-            timer2.Start();
+            timer2.Tick += Timer2_Tick; // Evento que se ejecutará cada vez que pase el tiempo establecido
+            timer2.Start(); // Iniciar el temporizador
         }
 
+        // Evento que se ejecuta cuando pasa el tiempo del primer temporizador
         private void Timer_Tick(object sender, EventArgs e)  
         {
-            timer.Stop();
+            timer.Stop();// Detener el temporizador para evitar que se repita continuamente
+
+            // Actualizar el texto en el label con información sobre el ajolote
             label1.Invoke((MethodInvoker)(() => label1.Text = "El ajolote es un anfibio endémico de México \n" +
             " que tiene una gran capacidad regenerativa y\n" +
             " puede regenerar extremidades perdidas y partes\n " +
@@ -49,12 +55,16 @@ namespace Axolotl_UN_Proyect
             "la contaminación de su hábitat, la caza para consumo humano y la\n" +
             "introducción de especies exóticas en su ambiente."));
 
+            // Actualizar la imagen en el pictureBox con una imagen relacionada al ajolote
             pictureBox1.Invoke((MethodInvoker)(() => pictureBox1.Image = Properties.Resources.Ajolote2));
         }
 
+        // Evento que se ejecuta cuando pasa el tiempo del segundo temporizador
         private void Timer2_Tick(object sender, EventArgs e)
         {
-            timer2.Stop();
+            timer2.Stop(); // Detener el temporizador para evitar que se repita continuamente
+
+            // Actualizar el texto en el label con otra información sobre el ajolote
             label1.Invoke((MethodInvoker)(() => label1.Text = "El hábitat natural del ajolote \n" +
             "es el lago de Xochimilco, cerca de la Ciudad de México. " +
             "Desafortunadamente, su conservación se encuentra en " +
@@ -73,12 +83,15 @@ namespace Axolotl_UN_Proyect
             "cuyos objetivos principales son prevenir y \n" +
             "reducir la vulnerabilidad en las especies \n" +
             "y sus hábitats y asegurar su conservación"));
+
+            // Actualizar la imagen en el pictureBox con otra imagen relacionada al ajolote
             pictureBox1.Invoke((MethodInvoker)(() => pictureBox1.Image = Properties.Resources.Ajolote3));
         }
 
 
         private void btnRegresar_Click_1(object sender, EventArgs e)
         {
+            // Cerrar la ventana actual y mostrar la ventana principal
             FormPrincipal principal = new FormPrincipal();
             this.Hide();
             principal.Show();
@@ -86,6 +99,7 @@ namespace Axolotl_UN_Proyect
 
         private void btnSiguiente_Click(object sender, EventArgs e)
         {
+            // Cerrar la ventana actual y mostrar la siguiente ventana
             AjoloteWord2 aWord2 = new AjoloteWord2();
             this.Hide();
             aWord2.Show();
