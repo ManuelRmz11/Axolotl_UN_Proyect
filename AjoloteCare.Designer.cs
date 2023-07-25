386e66e5b4815bf48f8a7d22bd3f5eafcd449e17
Ulisesdelvillar0207 <ur226278@gmail.com>
2023-07-25 09:50:38 -0600

    666

--- AjoloteCare.Designer.cs
+++ AjoloteCare.Designer.cs
@@ -71,7 +71,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(117, 100);
+            this.label1.Location = new System.Drawing.Point(115, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(516, 308);
             this.label1.TabIndex = 3;
