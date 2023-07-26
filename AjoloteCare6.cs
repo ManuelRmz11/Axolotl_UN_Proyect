38aa281bd64724c1dabc8aa3f44d984290b33fd5
ManuelRmz11 <116410273+ManuelRmz11@users.noreply.github.com>
2023-07-26 13:39:59 -0600

    Forms Nuevos

---
+++ AjoloteCare6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Axolotl_UN_Proyect
+{
+    public partial class AjoloteCare6 : Form
+    {
+        public AjoloteCare6()
+        {
+            InitializeComponent();
+        }
+    }
+}
