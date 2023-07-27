687a97891dde399beb5c3194f1c221271c22f77d
Ulisesdelvillar0207 <ur226278@gmail.com>
2023-07-26 18:39:54 -0600

    666

--- AjoloteCare5.cs
+++ AjoloteCare5.cs
@@ -12,9 +12,35 @@ namespace Axolotl_UN_Proyect
 {
     public partial class AjoloteCare5 : Form
     {
+        private string[] textos = { "Texto 1", "Texto 2", "Texto 3", "Texto 4", "Texto 5" };
+        private int currentIndex = 0;
+        private Timer timer;
+
         public AjoloteCare5()
         {
             InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 10000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Actualiza el texto del label con el texto actual y luego incrementa el índice para el siguiente texto
+            label1.Text = textos[currentIndex];
+
+            // Incrementa el índice asegurándose de que no exceda la longitud del arreglo
+            currentIndex = (currentIndex + 1) % textos.Length;
+        }
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            // Detiene el Timer antes de regresar a la pantalla anterior (si es necesario)
+            timer.Stop();
+
+            // Agrega aquí el código para regresar a la pantalla anterior
+
         }
     }
 }
